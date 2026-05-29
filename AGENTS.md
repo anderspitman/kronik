6fecae2c3e1d4b65aee77dc470af2a35af876b05
Anders Pitman <&ers@apitman.com>
2026-05-29 13:51:10 -0600

    Cleanup

--- AGENTS.md
+++
@@ -1,1 +0,0 @@
-Backup the database to a new folder in ./backups before any changes.
--- sandbox/run.sh
+++
@@ -1,7 +0,0 @@
-container_name=$(basename $PWD)
-
-podman run --rm -it \
-    --name ${container_name} \
-    -v $PWD:/mnt/${container_name} \
-    $@ \
-    kronik
